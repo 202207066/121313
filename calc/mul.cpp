4092c5500e19a9fb60fd3a50ac55de0b21fdd683
202207045 <alsxo0425@gmail.com>
2025-06-18 17:17:07 +0900

    인공수 정

--- calc/mul.cpp
+++ calc/mul.cpp
@@ -1,6 +1,11 @@
-#include "mul.h"
+내 브랜치에서 rebase 실행
 
-int mul(int x, int y)
-{
-	return 0;
-}
+bash
+git rebase origin/main
+현재 브랜치의 커밋들을 원격 main 브랜치의 최신 커밋 위에 재배치합니다.
+
+충돌 발생 시
+
+충돌 파일을 수정하고,
+
+bash
