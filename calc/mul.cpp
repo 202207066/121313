9fb74d641ea77768f184e1878f8ae0cfc8ac6a77
202207066 <hansol5818@naver.com>
2025-06-18 17:29:10 +0900

    11123

--- calc/mul.cpp
+++ calc/mul.cpp
@@ -1,11 +1,8 @@
 내 브랜치에서 rebase 실행
 
-bash
-git rebase origin/main
-현재 브랜치의 커밋들을 원격 main 브랜치의 최신 커밋 위에 재배치합니다.
-
-충돌 발생 시
-
-충돌 파일을 수정하고,
-
-bash
+int mul(int x, int y)
+{
+	return 0;
+}
+ang gimotti oh my kamisama suck ma dick 
+kimwonil JJANG
